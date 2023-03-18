5660842d77befdee0daf86a50e3b8373515ee8a0
Jeevan Bisht <jeevanb@hotmail.com>
2023-03-18 15:51:59 -0700

    Add files via upload

---
+++ QuickIPconfig.ps1
@@ -0,0 +1,12 @@
+﻿$netAdapter=Get-NetAdapter
+Get-NetIPAddress -InterfaceIndex $netAdapter.ifIndex  | Remove-NetIPAddress
+Get-NetRoute -InterfaceIndex 6 -DestinationPrefix "0.0.0.0/0" | Remove-NetRoute -ErrorAction SilentlyContinue
+
+
+New-NetIPAddress -InterfaceIndex $netAdapter.ifIndex `
+–IPAddress '192.168.10.2' `
+–PrefixLength 24 `
+-DefaultGateway '192.168.10.1'
+
+
+
